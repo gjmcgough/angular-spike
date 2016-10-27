1551f4820c40dc0d144a2e78ce1e66bfa8547712
Greg M <applecomputer@Apples-MacBook.local>
2016-10-27 12:27:07 -0700

    create post factory.

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}])
+
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
+'posts',
+function($scope, posts){
   $scope.posts = [
     {title: 'post 1', upvotes: 5},
     {title: 'post 2', upvotes: 2},
@@ -12,12 +20,19 @@ function($scope){
 
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
-    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0
+    });
     $scope.title = '';
+    $scope.link = '';
   };
 
   $scope.incrementUpvotes = function(post){
     post.upvotes += 1;
   };
+
+  $scope.posts = posts.posts;
 
 }]);

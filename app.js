2e49effb2edf39b75d858d3890ec80377027f857
Greg M <applecomputer@Apples-MacBook.local>
2016-10-27 11:10:50 -0700

    add posts repeat list and add post button.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-angular.module('flapperNews', [])
-.controller('MainCtrl', [
+var app = angular.module('flapperNews', []);
+app.controller('MainCtrl', [
 '$scope',
 function($scope){
   $scope.posts = [
-  'post 1',
-  'post 2',
-  'post 3',
-  'post 4',
-  'post 5'
+    {title: 'post 1', upvotes: 5},
+    {title: 'post 2', upvotes: 2},
+    {title: 'post 3', upvotes: 15},
+    {title: 'post 4', upvotes: 9},
+    {title: 'post 5', upvotes: 4}
 ];
+
+  $scope.addPost = function(){
+    $scope.posts.push({title: 'A new post!', upvotes: 0});
+  };
 }]);

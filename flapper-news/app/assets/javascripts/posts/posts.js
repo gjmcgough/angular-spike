d2a2680129ce1d45be8ec6ef59eba59ed46ab814
Greg M <applecomputer@Apples-MacBook.local>
2016-11-03 16:32:44 -0700

    Posts posting and persisting.

--- flapper-news/app/assets/javascripts/posts/posts.js
+++ flapper-news/app/assets/javascripts/posts/posts.js
@@ -6,12 +6,18 @@ app.factory('posts', [
   var o = {
     posts: []
   };
-  
+
   o.getAll = function() {
     return $http.get('/posts.json').success(function(data){
       angular.copy(data, o.posts);
     });
   };
 
+  o.create = function(post) {
+    return $http.post('/posts.json', post).success(function(data){
+      o.posts.push(data);
+    });
+  };
+  
   return o;
 }]);

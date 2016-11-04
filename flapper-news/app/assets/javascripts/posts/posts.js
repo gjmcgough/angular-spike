efad1d3153859d1298a43f87f20ed8022777eb0a
Greg M <applecomputer@Apples-MacBook.local>
2016-11-03 17:02:25 -0700

    upvotes persisting.

--- flapper-news/app/assets/javascripts/posts/posts.js
+++ flapper-news/app/assets/javascripts/posts/posts.js
@@ -8,16 +8,25 @@ app.factory('posts', [
   };
 
   o.getAll = function() {
-    return $http.get('/posts.json').success(function(data){
-      angular.copy(data, o.posts);
-    });
+    return $http.get('/posts.json')
+      .success(function(data){
+        angular.copy(data, o.posts);
+      });
   };
 
   o.create = function(post) {
-    return $http.post('/posts.json', post).success(function(data){
-      o.posts.push(data);
-    });
+    return $http.post('/posts.json', post)
+      .success(function(data){
+        o.posts.push(data);
+      });
   };
-  
+
+  o.upvote = function(post) {
+    return $http.put('/posts/' + post.id + '/upvote.json')
+      .success(function(data){
+        post.upvotes += 1;
+      });
+  };
+
   return o;
 }]);

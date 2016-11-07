d64ac82352e584ce76cb0fd49d327ec23b396f56
Greg M <applecomputer@Apples-MacBook.local>
2016-11-07 11:40:56 -0800

    Registration page throwing 401.

--- flapper-news/app/assets/javascripts/app.js
+++ flapper-news/app/assets/javascripts/app.js
@@ -17,14 +17,36 @@ app.config([
       });
     $stateProvider
       .state('posts', {
-          url: '/posts/{id}',
-          templateUrl: 'posts/_posts.html',
-          controller: 'PostsCtrl',
-          resolve: {
-            post: ['$stateParams', 'posts', function($stateParams, posts) {
-              return posts.get($stateParams.id);
-            }]
-          }
+        url: '/posts/{id}',
+        templateUrl: 'posts/_posts.html',
+        controller: 'PostsCtrl',
+        resolve: {
+          post: ['$stateParams', 'posts', function($stateParams, posts) {
+            return posts.get($stateParams.id);
+          }]
+        }
+      });
+    $stateProvider
+      .state('login', {
+        url: '/login',
+        templateUrl: 'auth/_login.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'Auth', function($state, Auth) {
+          Auth.currentUser().then(function (){
+            $state.go('home');
+          })
+        }]
+      });
+    $stateProvider
+      .state('register', {
+        url: '/register',
+        templateUrl: 'auth/_register.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'Auth', function($state, Auth) {
+          Auth.currentUser().then(function (){
+            $state.go('home');
+          })
+        }]
       });
     $urlRouterProvider.otherwise('home');
   }]);

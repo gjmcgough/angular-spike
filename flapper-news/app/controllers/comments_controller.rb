70d099ceefdc0e85025a9920aba6aeb467a15dc5
Greg M <applecomputer@Apples-MacBook.local>
2016-11-07 12:39:50 -0800

    add username next to comments and posts. site working.

--- flapper-news/app/controllers/comments_controller.rb
+++ flapper-news/app/controllers/comments_controller.rb
@@ -1,9 +1,9 @@
 class CommentsController < ApplicationController
   before_filter :authenticate_user!, only: [:create, :upvote]
-  
+
   def create
     post = Post.find(params[:post_id])
-    comment = post.comments.create(body: params[:body])
+    comment = post.comments.create(body: params[:body], user_id: current_user.id)
     respond_with post, comment
   end
 
